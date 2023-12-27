d860863f5282073d34f0a85cde360e2a01fcf627
navyasree229 <navyasree229@gmail.com>
2023-12-27 16:34:36 +0530

    Delete src/main/java/com/loop/qa directory

--- src/main/java/com/loop/qa/frameworkexception/FrameworkException.java
+++
@@ -1,9 +0,0 @@
-package com.loop.qa.frameworkexception;
-
-public class FrameworkException extends Exception{
-
-    public FrameworkException(String messg) {
-        super(messg);
-    }
-
-}
--- src/main/java/com/loop/qa/utils/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.loop.qa.utils;
-
-public class AppConstants {
-
-    public static final int SHORT_TIME_OUT = 5;
-    public static final int MEDIUM_TIME_OUT = 10;
-    public static final int LONG_TIME_OUT = 15;
-}
